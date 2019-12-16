c664b070e0f8a60c705286f55157866a72342eea
C. K. Tang <chuang.k.tang@vanderbilt.edu>
2019-12-16 17:09:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # copy .zshrc
-cp .zshrc ~/.zshrc
+cp ./.zshrc ~/.zshrc
+source ~/.zshrc
 
 # copy .albertignore
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
